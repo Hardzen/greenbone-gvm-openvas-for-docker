fac34d30ab05fcf89b42b4508651cd8b6114dbf5
Hardzen <Hardzen@gmail.com>
2023-03-08 01:51:16 +0100

    Update notus_scanner.sh

--- GVMDocker/scripts/debian/notus_scanner.sh
+++ GVMDocker/scripts/debian/notus_scanner.sh
@@ -9,7 +9,7 @@ export BUILD_DIR=$HOME/build
 mkdir -p "${BUILD_DIR}"
 
 export INSTALL_DIR=$HOME/install
-mkdir -p "${INSTALL_DIR}
+mkdir -p "${INSTALL_DIR}"
 
 curl -sSL https://github.com/greenbone/notus-scanner/archive/refs/tags/v${NOTUS_SCANNER_VERSION}.tar.gz -o ${SOURCE_DIR}/notus-scanner-${NOTUS_SCANNER_VERSION}.tar.gz
 tar -C ${SOURCE_DIR} -xvzf ${SOURCE_DIR}/notus-scanner-${NOTUS_SCANNER_VERSION}.tar.gz
