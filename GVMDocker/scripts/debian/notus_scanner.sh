f44cb9498294418648601b532a7b75fd3be99465
Hardzen <Hardzen@gmail.com>
2023-03-08 04:31:02 +0100

    Update notus_scanner.sh

--- GVMDocker/scripts/debian/notus_scanner.sh
+++ GVMDocker/scripts/debian/notus_scanner.sh
@@ -15,5 +15,6 @@ curl -sSL https://github.com/greenbone/notus-scanner/archive/refs/tags/v${NOTUS_
 tar -C ${SOURCE_DIR} -xvzf ${SOURCE_DIR}/notus-scanner-${NOTUS_SCANNER_VERSION}.tar.gz
 cd ${SOURCE_DIR}/notus-scanner-${NOTUS_SCANNER_VERSION}
 ls -l
-python3 -m pip install .
-
+mkdir -p $INSTALL_DIR/notus-scanner
+python3 -m pip install --prefix=$INSTALL_PREFIX --root=$INSTALL_DIR/notus-scanner --no-warn-script-location .
+sudo cp -rv $INSTALL_DIR/notus-scanner/* /
