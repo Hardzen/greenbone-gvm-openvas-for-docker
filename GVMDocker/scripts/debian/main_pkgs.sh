2a8ce9c262dce2ba12d08b55d59190012bb85800
Hardzen <Hardzen@gmail.com>
2023-03-08 03:47:25 +0100

    Update main_pkgs.sh

--- GVMDocker/scripts/debian/main_pkgs.sh
+++ GVMDocker/scripts/debian/main_pkgs.sh
@@ -105,7 +105,7 @@ sudo cp -rv ${INSTALL_DIR}/* /
 curl -sSL "https://github.com/greenbone/pg-gvm/archive/refs/tags/v${PGGVM_VERSION}.tar.gz" -o "${SOURCE_DIR}/pg-gvm-${PGGVM_VERSION}.tar.gz"
 ls -lahr "${SOURCE_DIR}"
 tar -C "${SOURCE_DIR}" -xvzf "${SOURCE_DIR}/pg-gvm-${PGGVM_VERSION}.tar.gz"
-mkdir -p ${BUILD_DIR}/PGGVM && cd ${BUILD_DIR}/pg-gvm-${PGGVM_VERSION}
+mkdir -p ${BUILD_DIR}/pg-gvm-${PGGVM_VERSION} && cd ${BUILD_DIR}/pg-gvm-${PGGVM_VERSION}
 
 cmake ${SOURCE_DIR}/pg-gvm-${PGGVM_VERSION} \
      -DCMAKE_INSTALL_PREFIX=${INSTALL_PREFIX} \
