014daf7fbe1db907f43de5c84a783d3b7f045fb9
Hardzen <Hardzen@gmail.com>
2023-03-08 01:10:26 +0100

    Create notus_scanner.sh

---
+++ GVMDocker/scripts/debian/notus_scanner.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+set -Eeuo pipefail
+
+curl -sSL https://github.com/greenbone/notus-scanner/archive/refs/tags/v${NOTUS_SCANNER_VERSION}.tar.gz -o ${SOURCE_DIR}/notus-scanner-${NOTUS_SCANNER_VERSION}.tar.gz
+tar -C ${SOURCE_DIR} -xvzf ${SOURCE_DIR}/notus-scanner-${NOTUS_SCANNER_VERSION}.tar.gz
+cd ${SOURCE_DIR}/notus-scanner-${NOTUS_SCANNER_VERSION}
+ls -l
+python3 -m pip install .
