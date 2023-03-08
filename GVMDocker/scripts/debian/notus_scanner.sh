e5b4802192129a299fb70eee6a8768de845585c5
Hardzen <Hardzen@gmail.com>
2023-03-08 01:31:01 +0100

    Update notus_scanner.sh

--- GVMDocker/scripts/debian/notus_scanner.sh
+++ GVMDocker/scripts/debian/notus_scanner.sh
@@ -1,11 +1,10 @@
 #!/usr/bin/env bash
 set -Eeuo pipefail
 
-apt install libpopt-dev
 
 curl -sSL https://github.com/greenbone/notus-scanner/archive/refs/tags/v${NOTUS_SCANNER_VERSION}.tar.gz -o ${SOURCE_DIR}/notus-scanner-${NOTUS_SCANNER_VERSION}.tar.gz
 tar -C ${SOURCE_DIR} -xvzf ${SOURCE_DIR}/notus-scanner-${NOTUS_SCANNER_VERSION}.tar.gz
 cd ${SOURCE_DIR}/notus-scanner-${NOTUS_SCANNER_VERSION}
 ls -l
 python3 -m pip install .
-apt remove libpopt-dev
+
